9edd3875b70118ffb9b59e9c80348064366282d1
Alexander Ivanov <alehander42@gmail.com>
2014-09-01 03:17:55 +0300

    Pack stuff

--- problems/lists.pl
+++ problems/lists.pl
@@ -33,7 +33,7 @@ flatten([], []).
 flatten(Element, [Element]) :- not(is_list(Element)).
 
 %% Eliminate consecutive duplicates of list elements.
-eliminate_cons_duplicates([Head|[Head|Tail]], List) :-
+eliminate_cons_duplicates([Head, Head|Tail], List) :-
   eliminate_cons_duplicates([Head|Tail], List).
 
 eliminate_cons_duplicates([Head, Other|Tail], List) :-
@@ -45,6 +45,33 @@ eliminate_cons_duplicates([Head], [Head]).
 
 eliminate_cons_duplicates([], []).
 
+%% Pack consecutive duplicates of list elements into sublists.
+pack_cons([Head|Tail], List) :-
+  pack_cons_([Head|Tail], [], List).
+
+pack_cons_([Head|Tail], Packet, List) :-
+  b(Packet, Head, C),
+  pack_cons_(Tail, C, List).
+
+pack_cons_([], Packet, List) :-
+  reverse(Packet, List).
+
+
+b([[Head|Tail]|List], Head, [[Head, Head|Tail]|List]).
+b([[Other|Q]|List], Head, [[Head], Q|List]) :- not(Head is Other).
+b([], Head, [[Head]]).
+
+%% Run-length encoding of a list.
+run_length(List, A) :-
+  pack_cons(List, Pack),
+  pack_length(Pack, A).
+
+pack_length([[Element|Elements]|Tail], A) :-
+  length([Element|Elements], B),
+  pack_length(Tail, TailLength),
+  append([[B, Element]], TailLength, A).
+
+pack_length([], []).
 
 
 
